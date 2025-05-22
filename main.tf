8ef5a1de5bdfbf92f83016d0cba854eb9885b04a
Grzegorz Malewicz <grzegorz.malewicz@gmail.com>
2025-05-22 19:04:18 +0200

    updated

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "digitalocean_kubernetes_cluster" "golf-test" {
 
   node_pool {
     name       = "default"
-    size       = "s-2vcpu-2gb"
+    size       = "s-2vcpu-2gb-amd"
     node_count = 1
   }
 }
